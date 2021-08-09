1273a6cc7f75ee7dfece553d6c45d6b73162d4bb
Gabriel Moraru <gabrielcmoraru@github.com>
2021-08-09 19:10:34 +0100

    override cache and stale time as default

--- client/src/components/treatments/hooks/useTreatments.ts
+++ client/src/components/treatments/hooks/useTreatments.ts
@@ -11,13 +11,7 @@ async function getTreatments(): Promise<Treatment[]> {
 
 export function useTreatments(): Treatment[] {
   const fallback = [];
-  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments, {
-    staleTime: 600000, // 10 minutes
-    cacheTime: 900000, // 15 minutes
-    refetchOnMount: false,
-    refetchOnWindowFocus: false,
-    refetchOnReconnect: false,
-  });
+  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments);
 
   return data;
 }
@@ -25,8 +19,5 @@ export function useTreatments(): Treatment[] {
 export function usePrefetchTreatments(): void {
   const queryClient = useQueryClient();
 
-  queryClient.prefetchQuery(queryKeys.treatments, getTreatments, {
-    staleTime: 600000, // 10 minutes
-    cacheTime: 900000, //
-  });
+  queryClient.prefetchQuery(queryKeys.treatments, getTreatments);
 }
