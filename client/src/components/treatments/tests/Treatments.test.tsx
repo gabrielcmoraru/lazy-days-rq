c562427340a0d43d6fade8fae98d29e830c9c6c3
Gabriel Moraru <gabrielcmoraru@github.com>
2021-08-11 17:22:50 +0100

    update queryClient testing wrapper to override some defaults, add some useQuery data and error tests

--- client/src/components/treatments/tests/Treatments.test.tsx
+++ client/src/components/treatments/tests/Treatments.test.tsx
@@ -3,6 +3,12 @@ import { screen } from '@testing-library/react';
 import { renderWithQueryClient } from '../../../test-utils';
 import { Treatments } from '../Treatments';
 
-test('renders response from query', () => {
+test('renders response from query', async () => {
   renderWithQueryClient(<Treatments />);
+
+  const treatmentsTitles = await screen.findAllByRole('heading', {
+    name: /massage|facial|scrub/i,
+  });
+
+  expect(treatmentsTitles).toHaveLength(3);
 });
