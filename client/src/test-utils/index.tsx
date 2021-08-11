548d56489588a1de17dbf5a7ae25665305f189f2
Gabriel Moraru <gabrielcmoraru@github.com>
2021-08-11 14:47:52 +0100

    add custom testing renderer that has provider by default for react query components

--- client/src/test-utils/index.tsx
+++ client/src/test-utils/index.tsx
@@ -1,8 +1,27 @@
-import { render } from '@testing-library/react';
+import { render, RenderResult } from '@testing-library/react';
+import { ReactElement } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 // import { defaultQueryClientOptions } from '../react-query/queryClient';
 
-// from https://tkdodo.eu/blog/testing-react-query#for-custom-hooks
+// unique query client for each test
+const generateQueryClient = () => {
+  return new QueryClient();
+};
+
+export function renderWithQueryClient(
+  ui: ReactElement,
+  client?: QueryClient,
+): RenderResult {
+  const queryClient = client ?? generateQueryClient();
+
+  return render(
+    <QueryClientProvider client={queryClient}>{ui}</QueryClientProvider>,
+  );
+}
+
+// ** For testing custom hooks **
+// // from https://tkdodo.eu/blog/testing-react-query#for-custom-hooks
 // export const createQueryClientWrapper = () => {
 //   const queryClient = generateQueryClient();
 //   return ({ children }) => (
